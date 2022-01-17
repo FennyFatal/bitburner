fad7ccc0746c9a6b55639438a839424e3810e920
FennyFatal <FennyFatal@users.noreply.github.com>
2022-01-17 08:39:20 -0700

    Update auto-hack.js

--- auto-hack.js
+++ auto-hack.js
@@ -12,7 +12,27 @@ export async function main(ns) {
 		scp,
 		exec,
 		sleep,
+		brutessh,
+		ftpcrack,
+		relaysmtp,
+		httpworm,
+		sqlinject,
+		fileExists,
+		getserver
 	} = ns;
+
+	const getOpenPortCount = () => {
+		const hackPrograms = [
+			'BruteSSH.exe',
+			'FTPCrack.exe',
+			'relaySMTP.exe',
+			'HTTPWorm.exe',
+			'SQLInject.exe'
+		];
+		let result = 0;
+		return hackPrograms.reduce((a, b) => a + (fileExists(b, "home") ? 1 : 0),0);
+	}
+
 	let startingServer = 0;
 	let allServers = scan(getHostname());
 	while (true) {
@@ -23,15 +43,30 @@ export async function main(ns) {
 		for (server = startingServer; server < servers.length; server++) {
 			await sleep(5)
 			if (getHackingLevel() >= getServerRequiredHackingLevel(servers[server])) {
-				if (hasRootAccess(servers[server]) == false && getServerNumPortsRequired(servers[server]) == 0) {
+				if (fileExists("BruteSSH.exe", "home")) {
+				    brutessh(servers[server]);
+				}
+				if (fileExists("FTPCrack.exe", "home")) {
+				    ftpcrack(servers[server]);
+				}
+				if (fileExists("relaySMTP.exe", "home")) {
+				    relaysmtp(servers[server]);
+				}
+				if (fileExists("HTTPWorm.exe", "home")) {
+				    httpworm(servers[server]);
+				}
+				if (fileExists("SQLInject.exe", "home")) {
+				    sqlinject(servers[server]);
+				}
+				if (hasRootAccess(servers[server]) == false && getServerNumPortsRequired(servers[server]) <= getOpenPortCount()) {
 					nuke(servers[server]);
 				}
 				if (hasRootAccess(servers[server]) == true) {
-					if (isRunning('loop-hack.script', servers[server]) == false) {
+					if (!isRunning('loop-hack.script', servers[server])) {
 						await scp('loop-hack.script', servers[server]);
 						exec('loop-hack.script', servers[server], 1);
 					}
-					if (isRunning('loop-grow.script', servers[server]) == false) {
+					if (!isRunning('loop-grow.script', servers[server])) {
 						await scp('loop-grow.script', servers[server]);
 						for (let t = 10; t > 0; t--) {
 							exec('loop-grow.script', servers[server], t);
